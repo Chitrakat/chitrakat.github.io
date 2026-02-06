dadab992a2e015c939ae33aeb09004e079a4b145
Suyash Chitrakar <schitrakar@knox.edu>
2026-02-06 10:51:49 -0600

    removed unused HTML files and made minor changes to landing page

--- circles.js
+++ circles.js
@@ -10,16 +10,15 @@ edited by Suyash Chitrakar
 let libs = ['https://unpkg.com/hydra-synth', 'includes/libs/hydra-synth.js', 'https://cdn.jsdelivr.net/gh/ffd8/hy5@main/hy5.js', 'includes/libs/hy5.js']
 
 // sandbox - start
-H.pixelDensity(0.5) // 2x = retina, set <= 1 if laggy
+H.pixelDensity(0.3) // 2x = retina, set <= 1 if laggy
 
 let random1, random2, random3;
 
 let maxCircle, minCircle;
 let mouseCircleSize;
 
-let randShade, randShade1, randShade2, opacity;
+let randShade, randShade1, randShade2;
 let value = 100;
-let o1 = 40;
 
 let n1;
 
@@ -27,15 +26,15 @@ s0.initP5() // send p5 to hydra
 P5.toggle(0) // hide p5
 
 src(s0)
-.add(src(o0).scale(0.6), 0.4) // controls the "glow"
-.modulateScale(noize(3), 0.9, 1 , 0.1) 
+.add(src(o0).scale(0.1), 0.4) // controls the "glow"
+.modulateScale(noize(1), 0.5, 2 , 100) 
 .out()
 // 1, 0.1, (0.05), 0.08, 1 and 1 
 // sandbox - end
 
 function windowResized() {
     resizeCanvas(windowWidth-10, windowHeight-10);
-    H.pixelDensity(0.5);
+    H.pixelDensity(0.3);
 }
 
 function setup() {
@@ -50,81 +49,58 @@ function setup() {
     random2 = random(width/3);
     random3 = random(width/3);
 
-    maxCircle = windowHeight/7;
-    minCircle = windowHeight/10;
-    mouseCircleSize = height/4;
+    maxCircle = windowHeight/8;
+    minCircle = windowHeight/15;
+    mouseCircleSize = height/8;
     
     randShade = random(100);
     randShade1 = random(100);
     randShade2 = random(100);
-    opacity = o1;
+    blendMode(HARD_LIGHT);
 }
 
 
 function draw() {
-    blendMode(HARD_LIGHT);
+    noStroke();
     if(mouseIsPressed){
         clear();
     }
-    
 
     // Colors for mouse circles
     let r = (sin(frameCount * 0.01) * 1.5 + 0.5) * randShade;
-    let g = (sin(frameCount * 0.003) * 0.5 + 0.5) * 10 + random(randShade1);
+    let g = (sin(frameCount * 0.003) * 0.5 + 0.5) * random(50, 100) + random(randShade1);
     let b = (sin(frameCount * 0.001) * 0.5 + 0.5) * 100 + randShade;
-    fill(r, g, b, 70); 
-    noStroke();
-    
-    // Mouse Circles
-    if (mouseX >= 0 && mouseX <= width && mouseY >= 0 && mouseY <= height) {
-        circle(mouseX, mouseY, mouseCircleSize);
-        // circle(windowWidth-mouseX, windowHeight-mouseY, mouseCircleSize % 100);
-    }
-    
+    fill(r, g, b); 
+    circle(mouseX, mouseY, mouseCircleSize);
+    // square(mouseX, mouseY, mouseCircleSize);
 
     // Middle Circle
     let b2 = ((sin(frameCount * 0.025) * 0.5 + 0.5) * randShade);
-    console.log(b2);
+    let rSize = (sin(frameCount * 0.009)) * maxCircle + minCircle;
     fill(180 -b2/2, 0, b2, 10);
-    noStroke();
-
-    let rSize = (sin(frameCount * 0.05)) * maxCircle + minCircle;
-    circle(width/2, height/2, height/2 + rSize);
-
+    circle(width/2, height/2, random3 + rSize);
+    // square(width/2, height/2, random3 + rSize);
 
     // Moving texts
     let tx, ty;
     let fontSize = height/5;
-    
-    // fill(200);
-
     tx = constrain(noise(100 + frameCount * 0.0006) * width, 0, width);
-    // Create moving objects if not already created
     if (!window.movingObjs) {
         window.movingObjs = [
             new movingObject(random(1000), random(1000), fontSize/2, 'Hi, I\'m Suyash'),
-            // new movingObject(random(1000), random(1000), fontSize/2, 'Hi, I\'m Suyash'),
-            
-            new movingObject(random(1000), random(1000), fontSize/2, 'I\'m a'),
-            new movingObject(random(1000), random(1000), fontSize/2, 'I\'m a'),
             new movingObject(random(1000), random(1000), fontSize/2, 'I\'m\n kinda lost'),
-            new movingObject(random(1000), random(1000), fontSize/2, 'graphic\ndesigner'),
-            new movingObject(random(1000), random(1000), fontSize/2, 'photographer'),
-            new movingObject(random(1000), random(1000), fontSize/2, 'photographer'),
-            new movingObject(random(1000), random(1000), fontSize/2, 'creative\ncoder'),
-            new movingObject(random(1000), random(1000), fontSize/2, 'artist?'),
-            new movingObject(random(1000), random(1000), fontSize/1.5, 'game\n designer'),
+            new movingObject(random(1000), random(1000), fontSize/2, 'a graphic\ndesigner'),
+            new movingObject(random(1000), random(1000), fontSize/2, 'a photographer'),
+            new movingObject(random(1000), random(1000), fontSize/2, 'a creative\ncoder'),
+            new movingObject(random(1000), random(1000), fontSize/2, 'an artist?'),
         ];
     }
 
     for (let obj of window.movingObjs) {
         obj.update();
-    }
-    for (let obj of window.movingObjs) {
         obj.display();
     }
     movingObject.separateAll(window.movingObjs);
-
 }
 
 class movingObject{
@@ -180,10 +156,10 @@ class movingObject{
         // Draw text
         rectMode(CENTER);
         fill(0);
-        this.xval = 0.006;
-        this.yval = 0.00065;
         stroke(0);
-        strokeWeight(4);
+        strokeWeight(2);
+        this.xval = 0.003;
+        this.yval = 0.00065;
         textAlign(CENTER, CENTER);
         text(this.label, this.x, this.y);
     }
